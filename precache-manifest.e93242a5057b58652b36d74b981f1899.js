c7ff148ebcaf8b76a840a05e0fc96a8e1576e4cf
pravzzyy <pravallika13kambhampati@gmail.com>
2020-08-21 19:41:01 +0530

    Updates

---
+++ precache-manifest.e93242a5057b58652b36d74b981f1899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee337b8ded6de67448d7fb55bb0850a0",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "3d0c3a688136863d0742",
+    "url": "/pokedex/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "22e5108d20d57d286d62",
+    "url": "/pokedex/static/css/main.d6086b54.chunk.css"
+  },
+  {
+    "revision": "3d0c3a688136863d0742",
+    "url": "/pokedex/static/js/2.99153eb0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex/static/js/2.99153eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e5108d20d57d286d62",
+    "url": "/pokedex/static/js/main.2ce56aaf.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
