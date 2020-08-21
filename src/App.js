95d83f899d83eebd4e218a8cd5e099e940b4bb64
pravzzyy <pravallika13kambhampati@gmail.com>
2020-08-21 19:39:38 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './components/layout/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Dashboard from './components/layout/Dashboard';
+import backgroundImage from './pattern.png';
+import Pokemon from './components/Pokemon/Pokemon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App" style={{background: `url(${backgroundImage})`}}>
+       <NavBar />
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/pokemon/:pokemonIndex" component={Pokemon} />
+          
+        </Switch>
+     </div>
     </div>
+    </Router>
+    
   );
 }
 
---
+++ src/components/Pokemon/PokemonCard.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Card = styled.div`
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    &:hover {
+        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+    -moz-user-select: none;
+    -website-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    -o-user-select: none;    
+}
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+
+`
+
+
+export default class PokemonCard extends Component {
+
+    state = {
+        name : '',
+        imageURL: '',
+        pokemonIndex: ''
+    }
+
+
+    componentDidMount() {
+        const { name, url } = this.props;
+        const pokemonIndex = url.split("/")[url.split("/").length -2];
+        const imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/a907856d8a243bc97fc8eb3b5d5fd61d593e28ec/sprites/pokemon/other/dream-world/${pokemonIndex}.svg`;
+        // https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png
+    this.setState({
+        name,
+        imageURL,
+        pokemonIndex
+    })
+
+    }
+
+
+
+
+    render() {
+
+       
+
+
+        return (
+            <div className="col-md-3 col-sm-6 mb-5">
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                <Card className="card">  
+                    <h5 className="card-header">{ this.state.pokemonIndex }</h5>
+                    <img className="card-img-tag rounded mx-auto mt-2"
+                        src={this.state.imageURL}
+                        style={{ height: "5em", width: "5em"}}
+                    />
+                    <div className="card-body mx-auto">
+                        <div className="card-title">
+                        {this.state.name}
+                        </div>
+                    </div>
+                </Card>
+                </StyledLink> 
+            </div>
+        )
+    }
+}
